28cd6c0cde86de928d4aece6b5680c46c5dfc0f2
focusdroid-python <focusdroid@163.com>
2021-12-25 21:57:20 +0800

    feat(add):添加sqlalchemy

--- manager.py
+++ manager.py
@@ -1,8 +1,24 @@
 
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+
+class Config(object):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:mysql@localhost:3306/info'
+    SQLALCHEMY_TRACK_MODIFICATIONS = Flask
+
+
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+def hello_world():
+    return 'hello'
 
 if __name__ == '__main__':
     app.run(debug=True)
