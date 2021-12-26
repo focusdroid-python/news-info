6399778a6e711a7e642cbaca501ed06215b3cc81
focusdroid-python <focusdroid@163.com>
2021-12-26 13:16:26 +0800

    feat(update):抽离config配置文件

---
+++ config.py
@@ -0,0 +1,49 @@
+from datetime import timedelta
+
+from flask import Flask
+from redis.client import StrictRedis
+
+
+class Config(object):
+    # 调试信息
+    DEBUG = True
+    SECRET_KEY = 'TBSEBFAF8998eb9afenajfv'
+
+    # mysql数据库配置信息
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:mysql@localhost:3306/info'
+    SQLALCHEMY_TRACK_MODIFICATIONS = Flask
+
+    # redis 配置信息
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
+
+    # Session配置信息
+    SESSION_TYPE = 'redis' # 设置session存储类型
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT) # 指定session存储的redis服务器
+    SESSION_USE_SIGNER = True # 设置签名存储
+    PERMANENT_SESSION_LIFETIME = timedelta(days=10) # 设置session有效期
+
+# 开发环境配置信息
+class DevelopConfig(Config):
+    pass
+
+# 生产环境配置信息
+class ProductConfig(Config):
+    DEBUG = False
+
+
+# 测试环境
+class TestConfig(Config):
+    pass
+
+
+# 提供统一的访问入口
+config_dict = {
+    "develop": DevelopConfig,
+    "peoduct": ProductConfig,
+    "test": TestConfig
+}
+
+
+
+
--- manager.py
+++ manager.py
@@ -1,33 +1,11 @@
-from datetime import timedelta
-
 from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 from flask_session import Session
 from flask_wtf.csrf import CSRFProtect
+from config import Config
 
 app = Flask(__name__)
-
-
-class Config(object):
-    # 调试信息
-    DEBUG = True
-    SECRET_KEY = 'TBSEBFAF8998eb9afenajfv'
-
-    # mysql数据库配置信息
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:mysql@localhost:3306/info'
-    SQLALCHEMY_TRACK_MODIFICATIONS = Flask
-
-    # redis 配置信息
-    REDIS_HOST = '127.0.0.1'
-    REDIS_PORT = 6379
-
-    # Session配置信息
-    SESSION_TYPE = 'redis' # 设置session存储类型
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT) # 指定session存储的redis服务器
-    SESSION_USE_SIGNER = True # 设置签名存储
-    PERMANENT_SESSION_LIFETIME = timedelta(days=10) # 设置session有效期
-
 
 
 app.config.from_object(Config)
